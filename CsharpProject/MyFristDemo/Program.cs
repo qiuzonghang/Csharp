d98059e8d0c7515823165b6d2e06eb66258b5ecf
qiuzonghang <2448140961@qq.com>
2022-10-24 21:31:01 +0800

    update

--- CsharpProject/MyFristDemo/Program.cs
+++ CsharpProject/MyFristDemo/Program.cs
@@ -8,7 +8,7 @@ namespace MyFristDemo       // 方案名
         static void Main(string[] args)     //Main函数/方法，程序运行主入口
         {
             // 打印至控制台
-            Console.WriteLine("This is My Frist Demo");
+            Console.WriteLine("This is My Frist Demo!");
 
             // 暂停当前程序，任意键关闭控制台
             Console.ReadKey();
