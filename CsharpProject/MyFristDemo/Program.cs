afb1e048389642df33e89469fb2f6d663f1f1034
qiuzonghang01 <qzh@qintelligence.cn>
2022-10-24 10:59:02 +0800

    update

--- CsharpProject/MyFristDemo/Program.cs
+++ CsharpProject/MyFristDemo/Program.cs
@@ -1,9 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
 
-namespace MyFristDemo
+namespace MyFristDemo       // 方案名
 {
-    class Program
+    class Program  
     {
         static void Main(string[] args)
         {
