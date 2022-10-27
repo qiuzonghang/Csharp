ceab48e7318a76e33faa5ee460b850c5b00ee4d7
qiuzonghang01 <qzh@qintelligence.cn>
2022-10-27 17:37:58 +0800

    update

---
+++ CsharpProject/OneDay/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+namespace OneDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Mimir";
+            int age = 18;
+            char gender = '男';
+            double heiht = 170.9;
+            decimal money = 10000m;
+            Console.WriteLine("我叫{0}，我今年{1}歲，我是{2}的，我身高{3}，工資{4}。", name, age, gender, heiht, money);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CsharpProject/TwoDay/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+namespace TwoDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 100;
+            int n2 = 50;
+            n1 = n1 - n2;
+            n2 = n2 + n1;
+            n1 = n2 - n1;
+            Console.WriteLine("n1:{0}\nn2:{1}",n1,n2);
+            Console.ReadKey();
+        }
+    }
+}
