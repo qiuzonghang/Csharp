4339450d25ade55792a0424b80a348092c60335c
qiuzonghang01 <qzh@qintelligence.cn>
2022-10-27 17:59:49 +0800

    update

---
+++ CsharpProject/OneDayFrist/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+namespace OneDayFrist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1 = 100.322;
+            double n2 = 20;
+            n1 = n1 - n2;
+            n2 = n1 + n2;
+            n1 = n2 - n1;
+            Console.WriteLine("n1:{0}\nn2:{1}", n1, n2);
+            Console.ReadKey();
+        }
+    }
+}
