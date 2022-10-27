4a32deb4f82dde0f68ed1ec84db0e3e4224718ae
qiuzonghang01 <qzh@qintelligence.cn>
2022-10-27 17:59:49 +0800

    update

---
+++ CsharpProject/ThreeDay/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+namespace ThtreeDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入个人信息");
+            string name = Console.ReadLine();
+            var nameList = name.Split(',');
+            Console.WriteLine("你的姓名：{0}", nameList[0]);
+            Console.ReadKey();
+        }
+    }
+}
