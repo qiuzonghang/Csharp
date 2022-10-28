249ad8c1b1a2dd98a99c26996a4dec5d5f96bba7
qiuzonghang01 <qzh@qintelligence.cn>
2022-10-28 17:29:33 +0800

    update

---
+++ CsharpProject/ThreeDayFrist/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+namespace ThreeDayFrist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Tshirt = 35;
+            int trousers = 120;
+            Console.WriteLine("小明共花费：{0}，打8.8折后为：{1}", Tshirt * 3 + trousers * 2, (Tshirt * 3 + trousers * 2) * 0.88);
+            Console.ReadKey();
+        }
+    }
+}
